1cf14dd7ca5b1d99aa2a3bb25a838ecf29aa9b74
johanmorales <johanmoralesrc1@gmail.com>
2024-03-06 16:20:29 -0600

    add filter by date

--- functions/get_purchases.sql
+++ functions/get_purchases.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION jmg.get_purchases()
+CREATE OR REPLACE FUNCTION jmg.get_purchases("start" TEXT, "end" TEXT)
 RETURNS TABLE (id INTEGER, description TEXT, cost NUMERIC(10,2), payment_method jmg.payment_method, store CITEXT, user_pay TEXT, user_purchase TEXT, status JMG.STATUS)
 AS $$
 DECLARE
@@ -19,6 +19,7 @@ BEGIN
     LEFT JOIN jmg.store s ON s.id = p.store_id
     LEFT JOIN jmg.user u ON u.id = p.user_pay_id
     LEFT JOIN jmg.user up ON up.id = p.user_purchase_id
+    WHERE p.created BETWEEN CONCAT("start", ' 00:00')::TIMESTAMP AND CONCAT("end", ' 23:59')::TIMESTAMP
     ORDER BY p.id;
     
 END;
