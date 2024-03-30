264d77bc8f607771d5a9120b2966ec31aeddc855
johanmorales <johanmoralesrc1@gmail.com>
2024-03-29 19:33:01 -0600

    add meses sin intereses

--- functions/create_purchase.sql
+++ functions/create_purchase.sql
@@ -3,6 +3,7 @@ RETURNS BOOLEAN
 AS $$
 DECLARE
     _purchase   jmg.purchase;
+    _months     INTEGER;
 BEGIN
 
     _purchase.description := purchase::JSONB ->> 'description';
@@ -12,9 +13,17 @@ BEGIN
     _purchase.user_pay_id := purchase::JSONB ->> 'user_pay';
     _purchase.user_purchase_id := purchase::JSONB ->> 'user_purchase';
     _purchase.status := purchase::JSONB ->> 'status';
+    _months := purchase::JSONB ->> 'months';
 
-    INSERT INTO jmg.purchase(description, cost, payment_method, store_id, user_pay_id, user_purchase_id, status)
-    VALUES(_purchase.description, _purchase.cost, _purchase.payment_method,_purchase.store_id, _purchase.user_pay_id, _purchase.user_purchase_id, _purchase.status);
+    IF _months = 1 THEN
+        INSERT INTO jmg.purchase(description, cost, payment_method, store_id, user_pay_id, user_purchase_id, status)
+        VALUES(_purchase.description, _purchase.cost, _purchase.payment_method,_purchase.store_id, _purchase.user_pay_id, _purchase.user_purchase_id, _purchase.status);
+    ELSE
+        FOR months IN 1.._months LOOP
+            INSERT INTO jmg.purchase(description, cost, payment_method, store_id, user_pay_id, user_purchase_id, status, created)
+            VALUES(CONCAT(_purchase.description, ' ', months), (_purchase.cost/_months), _purchase.payment_method,_purchase.store_id, _purchase.user_pay_id, _purchase.user_purchase_id, _purchase.status, NOW() + (months - 1) * INTERVAL '1 month');
+        END LOOP;
+    END IF;
 
     
     RETURN TRUE;
